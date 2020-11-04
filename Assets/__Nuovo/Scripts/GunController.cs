11a9c377fb00b8c63737fe384c1ee35d63214a48
nikisa <nikisa97@gmail.com>
2020-11-04 18:56:41 +0100

    Finito Movimento Base, Sparo ma senza fare danno

--- Assets/__Nuovo/Scripts/GunController.cs
+++ Assets/__Nuovo/Scripts/GunController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float damage;
+    public float distance;
+    public Camera mainCamera;
+
+    public float fireRatio = 5;
+    private float fireRatioTime;
+    public GameObject hitEffect;
+
+
+    
     void Update()
     {
-        
+
+        if (Input.GetButton("Fire1") && fireRatioTime <= Time.time)
+        {
+
+            fireRatioTime = Time.time + (1f / fireRatio);
+
+            //Sparo
+                RaycastHit hit;
+                if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, distance))
+                {
+                    Debug.Log(hit.transform.gameObject.name);
+                //Nemico.TakeDamage();
+
+                GameObject hitEffectObject = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(hitEffectObject, 1f);
+
+                }
+
+
+        }
+
     }
 }
--- Assets/__Nuovo/Scripts/PlayerMovement.cs
+++ Assets/__Nuovo/Scripts/PlayerMovement.cs
@@ -8,23 +8,49 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
     private Vector3 velocity;
-    public float gravity = 9.81f;
+    public float gravity = -9.81f;
+    public bool isTouch;
+    public float groundCheckerRadius = 0.2f;
+    public float forzaSalto;
     public CharacterController characterController;
     public LayerMask Terreno;
+    public Transform groundChecker;
+
+    public int maxJumps = 2;
+    private int jumpCounter = 0;
 
     
     void Start()
-    {
-        
-    }
+    {}
 
    
     void Update()
-    {                       
+    {
+
+        //Salto
+        isTouch = Physics.CheckSphere(groundChecker.position, groundCheckerRadius, Terreno);
+
+        if (isTouch && velocity.y < 0)
+        {
+            velocity.y = -1;
+            jumpCounter = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCounter < maxJumps)
+        {
+            velocity.y = Mathf.Sqrt(forzaSalto * gravity * -1);
+            jumpCounter++;
+        }
+
+
+        //Movimento
         float x = Input.GetAxis("Horizontal"); 
         float z = Input.GetAxis("Vertical");
         Vector3 movimento = transform.right * x + transform.forward * z;
         characterController.Move(movimento * Time.deltaTime * speed);
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
     }
 
     /*
@@ -35,5 +61,7 @@ public class PlayerMovement : MonoBehaviour
                 Extra: Abilità doppio salto.
     
      */
+
+    
 
 }
