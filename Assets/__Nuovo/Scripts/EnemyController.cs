735962137a998fe04a37cac1600dadeeb63ace16
nikisa <nikisa97@gmail.com>
2020-11-25 19:06:52 +0100

    Nav mesh added and baked on test scene

--- Assets/__Nuovo/Scripts/EnemyController.cs
+++ Assets/__Nuovo/Scripts/EnemyController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using Unity.Profiling;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
     [Range(0,100000)]
     public float life = 100;
+
+    public NavMeshAgent Agent;
+    public Transform positionReferencePlayer;
     
 
     public GameObject head;
@@ -16,6 +20,14 @@ public class EnemyController : MonoBehaviour
         //Aggiungere shader dissolvenza
         Destroy(transform.gameObject, 1f);
         transform.localScale = Vector3.zero; 
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.N))
+        {
+            Agent.SetDestination(positionReferencePlayer.position);
+        }
     }
 }
 
