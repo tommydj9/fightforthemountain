f4ed99b4ad690273d02cc7fdd7d7548e066255ef
DESKTOP-J3N4SJI\Zucca <tommasozucca6@gmail.com>
2021-05-26 18:43:29 +0200

    contunuo shop

--- Assets/bUTTONmANAGER.cs
+++ Assets/bUTTONmANAGER.cs
@@ -14,12 +14,18 @@ public class bUTTONmANAGER : MonoBehaviour
     [Header("Guns References")]
     public GunController sniper;
     public GunController rpg;
+    public GunController scar;
+    public GunController p90;
 
     [Header("Values")]
     [SerializeField]
     private int sniperCost = 20;
     [SerializeField]
     private int rpgCost = 100;
+    [SerializeField]
+    private int scarCost = 100;
+    [SerializeField]
+    private int P90Cost = 70;
 
     private void Start()
     {
@@ -99,6 +105,86 @@ public class bUTTONmANAGER : MonoBehaviour
                 Debug.Log("indexToChange: " + indexToChange);
                 player.SlotGun[rpg.gunInOrderID] = player.SlotGun.GetValue(indexToChange) as GameObject;
                 player.animator[rpg.gunInOrderID] = player.animator.GetValue(indexToChange) as Animator;
+                player.SlotGun[indexToChange] = tempGun;
+                player.animator[indexToChange] = tempAnimGun;
+            }
+
+        }
+    }
+
+
+    public void BuyScar()
+    {
+        Debug.Log("v-money click");
+        Debug.Log("player.totalCoins" + player.totalCoins + "scarCost" + scarCost);
+
+        if (player.totalCoins >= scarCost)
+        {
+            player.totalCoins = player.totalCoins - scarCost;
+
+            totalCoinsText.text = player.totalCoins.ToString();
+
+            if (!scar.isEquiped && player.idPosition < player.SlotGun.Length)
+            {
+                Debug.Log("scar equiped");
+                scar.isEquiped = true;
+
+                player.idPosition++;
+                scar.gunInOrderID = player.idPosition;
+                GameObject tempGun = player.SlotGun[scar.gunInOrderID];
+                Animator tempAnimGun = player.animator[scar.gunInOrderID];
+                int indexToChange = 0;
+
+                for (int j = 0; j < player.SlotGun.Length; j++)
+                {
+                    if (player.SlotGun[j].name == scar.name)
+                    {
+                        indexToChange = j;
+                    }
+                }
+                Debug.Log("indexToChange: " + indexToChange);
+                player.SlotGun[scar.gunInOrderID] = player.SlotGun.GetValue(indexToChange) as GameObject;
+                player.animator[scar.gunInOrderID] = player.animator.GetValue(indexToChange) as Animator;
+                player.SlotGun[indexToChange] = tempGun;
+                player.animator[indexToChange] = tempAnimGun;
+            }
+
+        }
+    }
+
+
+    public void BuyP90()
+    {
+        Debug.Log("v-money click");
+        Debug.Log("player.totalCoins" + player.totalCoins + "scarCost" + scarCost);
+
+        if (player.totalCoins >= scarCost)
+        {
+            player.totalCoins = player.totalCoins - P90Cost;
+
+            totalCoinsText.text = player.totalCoins.ToString();
+
+            if (!p90.isEquiped && player.idPosition < player.SlotGun.Length)
+            {
+                Debug.Log("scar equiped");
+                p90.isEquiped = true;
+
+                player.idPosition++;
+                p90.gunInOrderID = player.idPosition;
+                GameObject tempGun = player.SlotGun[p90.gunInOrderID];
+                Animator tempAnimGun = player.animator[p90.gunInOrderID];
+                int indexToChange = 0;
+
+                for (int j = 0; j < player.SlotGun.Length; j++)
+                {
+                    if (player.SlotGun[j].name == p90.name)
+                    {
+                        indexToChange = j;
+                    }
+                }
+                Debug.Log("indexToChange: " + indexToChange);
+                player.SlotGun[p90.gunInOrderID] = player.SlotGun.GetValue(indexToChange) as GameObject;
+                player.animator[p90.gunInOrderID] = player.animator.GetValue(indexToChange) as Animator;
                 player.SlotGun[indexToChange] = tempGun;
                 player.animator[indexToChange] = tempAnimGun;
             }
